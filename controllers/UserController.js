2455346ff206ec2f6b98d65590eb36136c7592a2
unknown <tywaynebuchanan@gmail.com>
2022-11-02 17:42:19 -0500

    made changes to usercontroller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
+
 const catchAsync = require("../utils/catchAsync")
+
 
 exports.getAllUsers = catchAsync(async (req,res,next) =>{
         const getAllUsers = await User.find();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
 
+
+const upload = multer({
+    dest: 'assets/img/users'
+})
+
 router.route("/users").get(UserController.getAllUsers);
 router.route("/user/:id").get(UserController.getUserById).patch(UserController.updateUser);
+
+// router.route("/updateMe",upload.single('photo'),UserController.updateMe);
 
 
 module.exports = router;
