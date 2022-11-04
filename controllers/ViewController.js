027f5de41e667aae12d38166860b84cd5acb15a2
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2022-11-04 15:32:27 -0500

    layout: made changes to the layout

--- controllers/ViewController.js
+++ controllers/ViewController.js
@@ -5,3 +5,15 @@ exports.LoginPage = ((req,res)=>{
 exports.Dashboard = ((req,res)=>{
     res.render('pages/dashboard');
 })
+
+exports.Preferences = ((req,res)=>{
+    res.render('pages/settings');
+})
+
+exports.Payments = ((req,res)=>{
+    res.render('pages/payments');
+})
+
+exports.Child = ((req,res)=>{
+    res.render('pages/child-info');
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,5 +6,8 @@ const AuthController = require("../controllers/AuthController");
 router.use(AuthController.isLoggedIn);
 router.route("/").get(ViewController.LoginPage);
 router.route("/dashboard").get(ViewController.Dashboard);
+router.route("/settings").get(ViewController.Preferences);
+router.route("/child-info").get(ViewController.Child);
+router.route("/payments").get(ViewController.Payments);
 
 module.exports = router
